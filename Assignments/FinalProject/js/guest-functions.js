6cc448442ba566d28464bd21e78223d52e905e70
SebastianHall69 <sebastianhall13@gmail.com>
2019-05-13 10:31:22 -0700

    Added and organized final project files

---
+++ Assignments/FinalProject/js/guest-functions.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+function updateName()
+{  //    ----------
+   // Set the new customer's name into local or session storage
+   // and redisplay the guest area using "setGuest" function
+    
+    
+}
+
+
+function clearName()
+{  //    ---------
+   // Reset the customer's name to nothing in local/session storage
+   // and redisplay the guest area using "set Guest" function
+	
+    
+}
+
+
+function setGuest()
+{  //    --------
+   // If the customer is not currrently signed in, call them "guest" 
+   // and give them a blank text box and a "sign in" button to use.
+   // If instead the customer already signed in earlier, welcome them by name 
+   // and give them a "sign out" buttom in case they want to use it. 
+   // Place the above mentioned items within the given "guest area" div.
+	
+    
+}
+
